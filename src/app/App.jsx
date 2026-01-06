ac296fdca0edee36666aeffaeb591cbeea3a5d04
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-06 22:27:56 +0530

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,1 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
+import LenderLayout from "../layouts/LenderLayout";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <LenderLayout>
+        <AppRoutes />
+      </LenderLayout>
+    </BrowserRouter>
+  );
+}
 
